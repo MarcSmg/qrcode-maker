bb2b453525a4763712c91d69d9fc969ef8ed8f98
Ely <martharun514@gmail.com>
2025-12-17 20:54:28 +0100

    Add font styles and create initial component files for QR code features

---
+++ frontend/EditionPages/QrDownload.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function QrDownload() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default QrDownload
---
+++ frontend/EditionPages/QrInfos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function QrInfos() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default QrInfos
