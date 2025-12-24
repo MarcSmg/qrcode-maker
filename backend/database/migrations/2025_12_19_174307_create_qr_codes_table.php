eefafa03c64738e33b29c6d85339594119777f6c
marc <dossagaby@gmail.com>
2025-12-24 16:22:34 +0100

    Added fields to the qr_codes migration

--- backend/database/migrations/2025_12_19_174307_create_qr_codes_table.php
+++ backend/database/migrations/2025_12_19_174307_create_qr_codes_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('qr_codes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')
+                    ->constrained('users');
+            $table->string('name');
+            $table->string('type');
+            $table->string('content');
             $table->timestamps();
         });
     }
