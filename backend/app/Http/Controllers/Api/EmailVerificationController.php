a3fdd7cde911f662da982d61c96f6452dee07d1e
marc <dossagaby@gmail.com>
2025-12-25 15:39:16 +0100

    Fixed email verification

--- backend/app/Http/Controllers/Api/EmailVerificationController.php
+++ backend/app/Http/Controllers/Api/EmailVerificationController.php
@@ -1,18 +1,30 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
 
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use App\Models\User;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Http\Request; 
 
-class EmailVerificationController
+class EmailVerificationController extends Controller
 {
-    public function __invoke(EmailVerificationRequest $request)
+    public function __invoke(Request $request, $id, $hash)
     {
-        if ($request->user()->hasVerifiedEmail()) {
+        $user = User::findOrFail($id);
+
+        // Vérification du hash
+        if (! hash_equals(sha1($user->email), $hash)) {
+            abort(403, 'Invalid verification link.');
+        }
+
+        if ($user->hasVerifiedEmail()) {
             return redirect(config('app.frontend_url') . '/');
         }
 
-        $request->fulfill();
+        $user->markEmailAsVerified();
+        event(new Verified($user));
 
         return redirect(config('app.frontend_url') . '/');
     }
